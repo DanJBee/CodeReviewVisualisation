cc9c3d0bc2e8b1c95a83f2df072bcc8f3f6c852f
Dan Bee <ZLAC198@live.rhul.ac.uk>
2024-06-05 09:39:01 +0100

    Edit schema.sql file again based on second set of feedback & updated schema ER diagram image file

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,100 +1,48 @@
 create table projects
 (
-    id         int,
+    id         int          primary key auto_increment,
     owner      varchar(100) not null,
-    repository varchar(100) not null
+    repository varchar(100) not null,
+    index projects_id_index on projects (id),
+    index projects_owner_index on projects (owner),
+    index projects_repository_index on projects (repository)
 );
-
-create index projects_id_index
-    on projects (id);
-
-create index projects_owner_index
-    on projects (owner);
-
-create index projects_repository_index
-    on projects (repository);
-
-alter table projects
-    add constraint projects_pk
-        primary key (id);
-
-alter table projects
-    modify id int auto_increment;
 
 create table pull_requests
 (
-    number            bigint       auto_increment,
+    number            bigint       primary key auto_increment,
     project_id        int          foreign key (project_id) references projects (id),
     created_at        timestamp    not null,
-    author_id         varchar(100) null,
-    type_name         varchar(100) null,
+    author_id         varchar(100) foreign key (author_id) references author (author_id),
+    type_name         varchar(100) foreign key (type_name) references author (type_name),
+    index pull_requests_author_id_index on pull_requests (author_id),
+    index pull_requests_id_index on pull_requests (project_id),
+    index pull_requests_number_index on pull_requests (number),
+    index pull_requests_type_name_index on pull_requests (type_name)
 );
-
-create index pull_requests_author_id_index
-    on pull_requests (author_id);
-
-create index pull_requests_id_index
-    on pull_requests (id);
-
-create index pull_requests_number_index
-    on pull_requests (number);
-
-create index pull_requests_type_name_index
-    on pull_requests (type_name);
-
-alter table pull_requests
-    add constraint pull_requests_pk
-        primary key (number);
 
 create table comments
 (
-    id                bigint       auto_increment,
+    id                bigint       primary key auto_increment,
     number            bigint       foreign key (number) references pull_requests (number),
     author_id         varchar(100) null,
     type_name         varchar(100) null,
     created_at        timestamp    not null,
+    index comments_author_id_index on comments (author_id),
+    index comments_number_index on comments (number),
+    index comments_type_name_index on comments (type_name)
 );
-
-create index comments_author_id_index
-    on comments (author_id);
-
-create index comments_number_index
-    on comments (number);
-
-create index comments_type_name_index
-    on comments (type_name);
-
-alter table comments
-    add constraint comments_pk
-        primary key (id);
 
 create table authors
 (
-    id                bigint,
+    id                bigint       primary key auto_increment,
     number            bigint       foreign key (number) references pull_requests (number),
     author_id         varchar(100) not null,
     author_avatar_url varchar(100) not null,
     type_name         varchar(100) not null,
+    index author_author_avatar_url_index on authors (author_avatar_url),
+    index author_author_id_index on authors (author_id),
+    index author_id_index on authors (id),
+    index author_number_index on authors (number),
+    index author_type_name_index on authors (type_name)
 );
-
-create index author_author_avatar_url_index
-    on authors (author_avatar_url);
-
-create index author_author_id_index
-    on authors (author_id);
-
-create index author_id_index
-    on authors (id);
-
-create index author_number_index
-    on authors (number);
-
-create index author_type_name_index
-    on authors (type_name);
-
-alter table authors
-    add constraint author_pk
-        primary key (id);
-
-alter table authors
-    modify id bigint auto_increment;
