f0c12da3b63deb14d6a1a9fbfaba9d79184c2dc4
DongGyun Han <DongGyun.Han@rhul.ac.uk>
2024-06-13 09:07:07 +0100

    add types

--- frontend/src/Graph.ts
+++ frontend/src/Graph.ts
@@ -1,13 +1,33 @@
 // Graph TypeScript file
 
+import React from "react";
+
 import {
+  SimulationLinkDatum,
+  SimulationNodeDatum,
   drag,
   forceCenter,
   forceLink,
   forceManyBody,
   forceSimulation,
   json,
-} from 'd3';
+} from "d3";
+
+interface Node extends SimulationNodeDatum {
+  id: string;
+  group?: string;
+}
+
+interface Link extends SimulationLinkDatum<Node> {
+  source: Node;
+  target: Node;
+  value: number;
+}
+
+interface Data {
+  nodes: Node[];
+  links: Link[];
+}
 
 const graph = () => {
   // Defines height & width values
@@ -15,66 +35,70 @@ const graph = () => {
   const width = window.innerWidth;
 
   return async (selection: any) => {
-    const data: any = await json('../miserables.json');
+    const data: Data | undefined = await json("../miserables.json");
+
+    if (!data) {
+      return;
+    }
 
     // Backup code for setting the nodes to a solid colour if needed
     // const colour = scaleOrdinal(schemeCategory10);
 
     // Defines the links & nodes in the graph
-    const links = data.links.map((d: any) => ({ ...d }));
-    const nodes = data.nodes.map((d: any) => ({ ...d }));
+    const links = data.links.map((d: Link) => ({ ...d }));
+    const nodes = data.nodes.map((d: Node) => ({ ...d }));
 
     // Defines the svg element that the graph will be within
     const svg = selection
-      .append('svg')
-      .attr('height', height)
-      .attr('width', width)
-      .attr('viewBox', [0, 0, width, height])
-      .attr('style', 'max-width: 100%; height: auto;');
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
 
     // Defines the link between each node in the graph
     const link = svg
-      .append('g')
-      .attr('stroke', '#999')
-      .attr('stroke-opacity', 1.5) // this number controls the thickness of each link
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 1.5) // this number controls the thickness of each link
       .selectAll()
       .data(links)
-      .join('line')
-      .attr('stroke-width', (d: any) => Math.sqrt(d.value));
+      .join("line")
+      .attr("stroke-width", (d: Link) => Math.sqrt(d.value));
 
     // Defines each individual node in the graph
     const node = svg
-      .append('g')
-      .attr('stroke', '#fff')
-      .attr('stroke-width', 1.5)
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
       .selectAll()
       .data(nodes)
-      .join('circle')
-      .attr('r', 15) // this number controls the size of each node
+      .join("circle")
+      .attr("r", 15) // this number controls the size of each node
       // Backup code for when nodes are filled with a solid colour if needed
       // .attr('fill', (d) => colour(d.group))
-      .attr('fill', 'url(#image)');
+      .attr("fill", "url(#image)");
 
     // Sets the position attribute of the links & nodes in the graph each time the nodes 'ticks'
     function ticked() {
       link
-        .attr('x1', (d: any) => d.source.x)
-        .attr('y1', (d: any) => d.source.y)
-        .attr('x2', (d: any) => d.target.x)
-        .attr('y2', (d: any) => d.target.y);
+        .attr("x1", (d: Link) => d.source.x)
+        .attr("y1", (d: Link) => d.source.y)
+        .attr("x2", (d: Link) => d.target.x)
+        .attr("y2", (d: Link) => d.target.y);
 
-      node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y);
+      node.attr("cx", (d: Node) => d.x).attr("cy", (d: any) => d.y);
     }
 
     // Defines the simulation of the physics on the nodes in the graph
     const simulation = forceSimulation(nodes)
       .force(
-        'link',
-        forceLink(links).id((d: any) => d.id),
+        "link",
+        forceLink(links).id((d: any) => d.id)
       )
-      .force('charge', forceManyBody())
-      .force('centre', forceCenter(width / 2, height / 2))
-      .on('tick', ticked);
+      .force("charge", forceManyBody())
+      .force("centre", forceCenter(width / 2, height / 2))
+      .on("tick", ticked);
 
     // Reheats the simulation when the drag starts & fix the subject's i.e. the node's position
     function started(event: any) {
@@ -102,34 +126,34 @@ const graph = () => {
     }
 
     // Defines the element for the avatar URl pattern on the nodes in the graph
-    const defs = svg.append('defs');
+    const defs = svg.append("defs");
 
     // Defines the avatar URL pattern themselves on the nodes in the graph
     const pattern = defs
-      .append('pattern')
-      .attr('id', 'image')
-      .attr('x', '0')
-      .attr('y', '0')
-      .attr('height', '1')
-      .attr('width', '1');
+      .append("pattern")
+      .attr("id", "image")
+      .attr("x", "0")
+      .attr("y", "0")
+      .attr("height", "1")
+      .attr("width", "1");
 
     // Adds the avatar URL pattern to the nodes in the graph
     pattern
-      .append('image')
-      .attr('x', '0')
-      .attr('y', '0')
-      .attr('height', '30') // double the radius value
-      .attr('width', '30') // double the radius value
+      .append("image")
+      .attr("x", "0")
+      .attr("y", "0")
+      .attr("height", "30") // double the radius value
+      .attr("width", "30") // double the radius value
       .attr(
-        'xlink:href',
-        'https://avatars.githubusercontent.com/u/22572315?v=4', // this value controls the image on the nodes in the graph
+        "xlink:href",
+        "https://avatars.githubusercontent.com/u/22572315?v=4" // this value controls the image on the nodes in the graph
       );
 
     // Appends a title to each node
-    node.append('title').text((d: any) => d.id);
+    node.append("title").text((d: any) => d.id);
 
     // Applies the drag physics to each node in the graph
-    node.call(drag().on('start', started).on('drag', dragged).on('end', ended));
+    node.call(drag().on("start", started).on("drag", dragged).on("end", ended));
 
     // Returns the svg node
     return svg.node();
