cb9106a0c0d78d5934ba189a5c985fddd521f193
Dan Bee <ZLAC198@live.rhul.ac.uk>
2024-06-13 16:20:29 +0100

    Edit main.tsx, add moment.js dependency, & remove day.js dependency based on initial feedback

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import 'dayjs/locale/en-gb';
+import 'moment/locale/en-gb';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import GraphComponent from './GraphComponent';
 import TimePeriodSelectorComponent from './TimePeriodSelectorComponent';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GraphComponent />
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
       <TimePeriodSelectorComponent />
     </LocalizationProvider>
   </React.StrictMode>,
