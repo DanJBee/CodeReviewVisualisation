898e70bbe05c6184cde47e4f280748625e6b2b30
Dan Bee <ZLAC198@live.rhul.ac.uk>
2024-06-14 14:27:22 +0100

    Edit BackendApplicationTests.java to fix formatting

--- backend/src/test/java/danbee/frontend/BackendApplicationTests.java
+++ backend/src/test/java/danbee/frontend/BackendApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FrontendApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
