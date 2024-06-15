25def37c814e77c26059be159d8fe9d20d6b504a
Dan Bee <ZLAC198@live.rhul.ac.uk>
2024-06-15 11:07:58 +0100

    Add ConfigurationClass.java to store database credentials

---
+++ backend/src/main/java/danbee/frontend/configurations/ConfigurationClass.java
@@ -0,0 +1,14 @@
+package danbee.frontend.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * General configuration class for database credentials in particular.
+ *
+ * @author Dan Bee
+ */
+@Configuration
+@PropertySource("classpath:database.properties")
+public class ConfigurationClass {
+}
